b840b1b41980fbbf210e7fddd5bbc4c8d213d69a
Richet <shanon.richet@gmail.com>
2021-11-19 13:56:56 +0100

    add main and header

---
+++ src/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header= () => {
+    return(
+        <header>
+        <input type= "text" placeholder= "add ToDoList" />    
+        </header>
+    )
+}
+
+export default Header
---
+++ src/Main/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Main= () => {
+    return(
+        <main>
+        <ul>
+        <li>
+        <input type="checkbox" /> My first todo
+        </li>
+        <li>
+        <input type="checkbox" /> My first todo
+        </li>
+        </ul>
+        </main>
+    )
+}
+
+export default Main
+
+  
