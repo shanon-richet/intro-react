c8fcd6e44f4b9ebdba1994883b018a550d1ef35e
Richet <shanon.richet@gmail.com>
2021-11-22 16:12:31 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
+import React, { useState, useRef } from 'react';
 import './App.css';
-import Header from './Header';
-import Main from './Main';
+import TodoList from './TodoList';
 
 function App() {
+  const inputRef= useRef();
+  function clickHandler() {
+    const value= inputRef.current;
+    console.log(value.value)
+  }
+ 
   return (
-    <div className="App">
-          <Header />
-          <Main />     
-    </div>
+    <>
+    <h1></h1>
+      <form>
+          <input ref= {inputRef} placeholder= "write todo" type= "text">
+          </input>
+          <button onClick= {clickHandler} >add</button>
+      </form>  
+      <TodoList />
+    </>
   );
 }
-
 export default App;
