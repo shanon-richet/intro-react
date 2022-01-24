778015e0ebcc6276e11bdfb62a118dc57e2f5331
shanon-richet <89406450+shanon-richet@users.noreply.github.com>
2022-01-24 13:43:46 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './App.css';
 import TodoList from './TodoList';
 
 function App() {
-  const inputRef= useRef();
-  function clickHandler() {
-    const value= inputRef.current;
-    console.log(value.value)
+  const initial = [{
+    "task": "Eat",
+    "complete": false
+  }, {
+    "task": "Sleep",
+    "complete": true
+  }]
+  const [todos, setTodos] = useState(initial)
+
+  const inputRef = useRef();
+
+  function clickHandler(e) {
+    e.preventDefault()
+
+    setTodos(previous => {
+      return [...previous, {
+        "task": inputRef.current.value,
+        "complete": false
+      }]
+    })
+    console.log(todos)
+
   }
- 
+
+  useEffect(() => {
+    // storing input name
+    localStorage.setItem("name", JSON.stringify(todos));
+  }, [todos]);
+
   return (
     <>
-    <h1></h1>
+      <h1>TodoList</h1>
       <form>
-          <input ref= {inputRef} placeholder= "write todo" type= "text">
-          </input>
-          <button onClick= {clickHandler} >add</button>
-      </form>  
+        <input ref={inputRef} placeholder="write todo" type="text" />
+        <button onClick={(e) => (clickHandler(e))} >add</button>
+      </form>
       <TodoList />
+      <div>
+        <ul>
+          {
+            todos.map((todo, index) => (
+              <li>
+                <input key={index} type="checkbox" defaultChecked={todo.complete} /> {todo.task}
+              </li>
+            ))}
+        </ul>
+      </div>
     </>
   );
 }
